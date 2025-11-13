7911c72b2ff06811d7ab49cc4cbc139c5518c2ae
POMPY PRODUCTIONS <accounts@pompyproductions.com>
2025-11-13 16:56:00 +0100

    test strncmp

--- test_strncmp.c
+++ test_strncmp.c
@@ -9,12 +9,49 @@ static MunitResult test_basic(const MunitParameter params[], void* data)
 		ft_strncmp("hello", "hel10", 5), 
 		strncmp("hello", "hel10", 5)
 	));
+	munit_assert_true(are_same_sign(
+		ft_strncmp("hello", "hel10", 3), 
+		strncmp("hello", "hel10", 3)
+	));
+	munit_assert_true(are_same_sign(
+		ft_strncmp("hello", "hel10", 0), 
+		strncmp("hello", "hel10", 0)
+	));
+	munit_assert_true(are_same_sign(
+		ft_strncmp("hello", "hello42", 7), 
+		strncmp("hello", "hello42", 7)
+	));
 
+	return (MUNIT_OK);
+}
+
+static MunitResult test_edge(const MunitParameter params[], void* data)
+{
+	(void) params; (void) data;
+
+	munit_assert_true(are_same_sign(
+		ft_strncmp("", "", 0), 
+		strncmp("", "", 0)
+	));
+	munit_assert_true(are_same_sign(
+		ft_strncmp("", "", 3), 
+		strncmp("", "", 3)
+	));
+	munit_assert_true(are_same_sign(
+		ft_strncmp("hello", "", 3), 
+		strncmp("hello", "", 3)
+	));
+	munit_assert_true(are_same_sign(
+		ft_strncmp("", "hello", 7), 
+		strncmp("", "hello", 7)
+	));
+	
 	return (MUNIT_OK);
 }
 
 static MunitTest tests[] = {
 	{ "/basic", test_basic, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+	{ "/edge", test_edge, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
 	{ NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
 };
 
